71ca97192305f0dcf5ed0aee52469be8eaf6c137
simplegitops1 <chanchalraut30@gmail.com>
2024-07-25 21:06:12 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 252612
+let accountEmail = "chanchal@gmail.com"
+var accountPassword = 123456
+accountCountry = "kolkata"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCountry])
